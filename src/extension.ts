13d14dc2e0c6f4842aaa02bce544c963d9f74262
Celery Liu <liuct94@gmail.com>
2023-03-15 20:51:16 +0800

    create a html page to explain code

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import axios from "axios";
 import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,8 +12,6 @@ export function activate(context: vscode.ExtensionContext) {
 
   const document = editor!.document;
   const languageId = document.languageId;
-
-  let panel: vscode.WebviewPanel | undefined = undefined;
 
   let addComments = vscode.commands.registerCommand("openai-code.addComments", () => {
     sendRequest("Add Documentation comments to the code (" + languageId + "):").then((response) => {
@@ -39,25 +39,39 @@ export function activate(context: vscode.ExtensionContext) {
 
   let askToAI = vscode.commands.registerCommand("openai-code.askToAI", () => {
     // Create and show a new webview
-    if (!panel) {
-      panel = vscode.window.createWebviewPanel(
-        'chatCoding', // Identifies the type of the webview. Used internally
-        'Chat Coding', // Title of the panel displayed to the user
-        vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
-        {} // Webview options. More on these later.
-      );
-    }
+    const panel = vscode.window.createWebviewPanel(
+      "chatCoding", // Identifies the type of the webview. Used internally
+      "Chat Coding", // Title of the panel displayed to the user
+      vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+      {
+        enableScripts: true,
+      } // Webview options. More on these later.
+    );
     const selectionText = editor!.document.getText(editor!.selection);
 
-    panel.webview.html = getWebviewContent(htmlEscape(selectionText));
+    const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, "src", "index.html"));
+    const fileContent = fs.readFileSync(filePath.fsPath, "utf8");
 
-    sendRequest("Explain this code:").then((response) => {
-      if (!response) {
-        return;
-      }
+    panel.webview.html = fileContent.replace("{{code}}", htmlEscape(selectionText));
+    panel.webview.onDidReceiveMessage(
+      (message) => {
+        console.log(message);
 
-      panel!.webview.postMessage({ response });
-    });
+        switch (message.msg) {
+          case "init":
+            sendRequest("Explain this code:", selectionText).then((response) => {
+              if (!response) {
+                return;
+              }
+
+              panel!.webview.postMessage({ response: response.replace(/\n/g, "<br/>") });
+            });
+            return;
+        }
+      },
+      undefined,
+      context.subscriptions
+    );
   });
 
   context.subscriptions.push(addComments, refactorCode, askToAI);
@@ -66,51 +80,17 @@ export function activate(context: vscode.ExtensionContext) {
 function htmlEscape(str: string) {
   return str.replace(/[&<>'"]/g, (tag) => {
     const map: any = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      "'": '&#39;',
-      '"': '&quot;'
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      "'": "&#39;",
+      '"': "&quot;",
     };
     return map[tag] || tag;
   });
 }
 
-
-function getWebviewContent(code: string) {
-  return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cat Coding</title>
-</head>
-<body>
-  <h5>Select Content:</h5>
-  <pre>
-    <code>
-      ${code}
-    </code>
-  </pre>
-  <h5>Explain from OpenAI Code Chat Assistant:</h5>
-  <p id="result"></p>
-  <script>
-  const result = document.getElementById('result');
-
-  // Handle the message inside the webview
-  window.addEventListener('message', event => {
-
-      const message = event.data; // The JSON data our extension sent
-
-      result.innerHTML = message.response;
-      }
-  });
-  </script>
-</body>
-</html>`;
-}
-
-async function sendRequest(promptPrefix: string) {
+async function sendRequest(promptPrefix: string, selectionText: string = "") {
   const config = vscode.workspace.getConfiguration("openAICode");
   const apiKey = config.get("apiKey") as string | undefined;
   const endpoint = config.get("endpoint") as string | undefined;
@@ -125,28 +105,28 @@ async function sendRequest(promptPrefix: string) {
     return;
   }
 
-  let editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    vscode.window.showInformationMessage("No editor is active.");
-    return;
+  if (!selectionText) {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.showInformationMessage("No editor is active.");
+      return;
+    }
+    let selection = editor.selection;
+    if (selection.isEmpty) {
+      vscode.window.showInformationMessage("Please select some text.");
+      return;
+    }
+    selectionText = editor.document.getText(selection);
   }
-  let selection = editor.selection;
-  if (selection.isEmpty) {
-    vscode.window.showInformationMessage("Please select some text.");
-    return;
-  }
-  let selectedText = editor.document.getText(selection);
 
   let postData = {
-    prompt: `<|im_start|>system\nYou are an AI assistant that helps people find information.\n<|im_end|>\n<|im_start|>user\n${promptPrefix}\n${selectedText}<|im_end|>\n<|im_start|>assistant`,
-    "temperature": 0.5,
-    "top_p": 0.95,
-    "frequency_penalty": 0,
-    "presence_penalty": 0,
-    "max_tokens": 800,
-    stop: [
-      "<|im_end|>"
-    ]
+    prompt: `<|im_start|>system\nYou are an AI assistant that helps people find information.\n<|im_end|>\n<|im_start|>user\n${promptPrefix}\n${selectionText}<|im_end|>\n<|im_start|>assistant`,
+    temperature: 0.5,
+    top_p: 0.95,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 800,
+    stop: ["<|im_end|>"],
   };
 
   let requestConfig = {
@@ -172,4 +152,4 @@ async function sendRequest(promptPrefix: string) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
