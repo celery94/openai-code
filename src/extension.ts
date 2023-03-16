6b368f20e863276e9c57dd8ec2dab7d83ae77da4
Celery Liu <liuct94@gmail.com>
2023-03-16 21:59:50 +0800

    only keep ask to AI

--- src/extension.ts
+++ src/extension.ts
@@ -5,38 +5,13 @@ import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
 import * as util from "util";
+import showdown = require("showdown");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   let editor = vscode.window.activeTextEditor;
-
-  const document = editor!.document;
-  const languageId = document.languageId;
-
-  let addComments = vscode.commands.registerCommand("openai-code.addComments", () => {
-    sendRequest("Add Documentation comments to the code (" + languageId + "):").then((response) => {
-      if (!response) {
-        return;
-      }
-
-      editor!.edit((editBuilder) => {
-        editBuilder.replace(editor!.selection, response!);
-      });
-    });
-  });
-
-  let refactorCode = vscode.commands.registerCommand("openai-code.refactorCode", () => {
-    sendRequest("Refactor this code and use comment explain what's changed:").then((response) => {
-      if (!response) {
-        return;
-      }
-
-      editor!.edit((editBuilder) => {
-        editBuilder.replace(editor!.selection, response!);
-      });
-    });
-  });
+  const languageId = editor!.document.languageId;
 
   let askToAI = vscode.commands.registerCommand("openai-code.askToAI", () => {
     // Create and show a new webview
@@ -49,7 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
       } // Webview options. More on these later.
     );
 
-    const bulma = panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'media', 'bulma.min.css'));
+    const bulma = panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, "media", "bulma.min.css"));
     const selectionText = editor!.document.getText(editor!.selection);
 
     const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, "src", "index.html"));
@@ -58,18 +33,16 @@ export function activate(context: vscode.ExtensionContext) {
     panel.webview.html = util.format(fileContent, bulma, htmlEscape(selectionText));
     panel.webview.onDidReceiveMessage(
       (message) => {
-        console.log(message);
+        if (message.msg) {
+          sendRequest(selectionText, languageId, message.msg).then((response) => {
+            if (!response) {
+              return;
+            }
+            const converter = new showdown.Converter();
+            const html = converter.makeHtml(response);
 
-        switch (message.msg) {
-          case "init":
-            sendRequest("Explain this code:", selectionText).then((response) => {
-              if (!response) {
-                return;
-              }
-
-              panel!.webview.postMessage({ response: response.replace(/\n/g, "<br/>") });
-            });
-            return;
+            panel!.webview.postMessage({ response: html });
+          });
         }
       },
       undefined,
@@ -77,7 +50,7 @@ export function activate(context: vscode.ExtensionContext) {
     );
   });
 
-  context.subscriptions.push(addComments, refactorCode, askToAI);
+  context.subscriptions.push(askToAI);
 }
 
 function htmlEscape(str: string) {
@@ -93,7 +66,7 @@ function htmlEscape(str: string) {
   });
 }
 
-async function sendRequest(promptPrefix: string, selectionText: string = "") {
+async function sendRequest(selectionText: string, languageId: string, userQuery: string) {
   const config = vscode.workspace.getConfiguration("openAICode");
   const apiKey = config.get("apiKey") as string | undefined;
   const endpoint = config.get("endpoint") as string | undefined;
@@ -108,22 +81,13 @@ async function sendRequest(promptPrefix: string, selectionText: string = "") {
     return;
   }
 
-  if (!selectionText) {
-    let editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      vscode.window.showInformationMessage("No editor is active.");
-      return;
-    }
-    let selection = editor.selection;
-    if (selection.isEmpty) {
-      vscode.window.showInformationMessage("Please select some text.");
-      return;
-    }
-    selectionText = editor.document.getText(selection);
-  }
+  const systemStart = `<|im_start|>system\nYou are an AI assistant that helps people find information.
+    Here is a ${languageId} snippet :
+    ${selectionText}
+    <|im_end|>\n`;
 
   let postData = {
-    prompt: `<|im_start|>system\nYou are an AI assistant that helps people find information.\n<|im_end|>\n<|im_start|>user\n${promptPrefix}\n${selectionText}<|im_end|>\n<|im_start|>assistant`,
+    prompt: `${systemStart}<|im_start|>user\n${userQuery}\n<|im_end|>\n<|im_start|>assistant`,
     temperature: 0.5,
     top_p: 0.95,
     frequency_penalty: 0,
@@ -150,8 +114,7 @@ async function sendRequest(promptPrefix: string, selectionText: string = "") {
     console.error(error);
     vscode.window.showErrorMessage("Error: " + error);
     return;
-  }
-  finally {
+  } finally {
     statusBarItem.hide();
   }
 
@@ -163,4 +126,4 @@ async function sendRequest(promptPrefix: string, selectionText: string = "") {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
